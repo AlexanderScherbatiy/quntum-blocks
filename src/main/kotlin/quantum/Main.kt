0d786dcf99a42bdb5e0631f662774105ba752ec5
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2018-10-29 19:55:21 +0300

    Inital commit

---
+++ src/main/kotlin/quantum/Main.kt
@@ -0,0 +1,10 @@
+package quantum
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args : Array<String>) {
+            println("Hello, Quantum Blocks!")
+        }
+    }
+}
